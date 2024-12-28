b912080f4b7228aea54f42b66becca2ae496ea1c
felipeCast <castfelipe10@gmail.com>
2024-12-28 16:52:24 -0300

    finalizando menu desktop e mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//Header trocar de cor ao deslizar
+
 function scrollHeader(){
     const nav = document.getElementById('header');
 
@@ -6,3 +8,70 @@ function scrollHeader(){
 
 //Chamando a função ao carregar 
 window.addEventListener('scroll', scrollHeader);
+
+//Dropdown
+
+const dropdownMenu1 = document.getElementById('dropdown-item-1');
+const dropdownMenu2 = document.getElementById('dropdown-item-2');
+const dropdownMenu3 = document.getElementById('dropdown-item-3');
+
+const menuItem1 = document.getElementById('menu-item-1');
+const menuItem2 = document.getElementById('menu-item-2');
+const menuItem3 = document.getElementById('menu-item-3');
+
+function ShowDropdown1(event){
+    event.preventDefault();
+    
+    //Ao clicar em um, os outros não irão aparecer;
+    dropdownMenu1.classList.toggle('active-dropdown-menu');
+    dropdownMenu2.classList.remove('active-dropdown-menu');
+    dropdownMenu3.classList.remove('active-dropdown-menu');
+}
+
+function ShowDropdown2(event){
+    event.preventDefault();
+    
+    dropdownMenu1.classList.remove('active-dropdown-menu');
+    dropdownMenu2.classList.toggle('active-dropdown-menu');
+    dropdownMenu3.classList.remove('active-dropdown-menu');
+}
+
+function ShowDropdown3(event){
+    event.preventDefault();
+    
+    dropdownMenu1.classList.remove('active-dropdown-menu');
+    dropdownMenu2.classList.remove('active-dropdown-menu');
+    dropdownMenu3.classList.toggle('active-dropdown-menu');
+}
+
+function RemoveDropdown(){
+    dropdownMenu1.classList.remove('active-dropdown-menu');
+    dropdownMenu2.classList.remove('active-dropdown-menu');
+    dropdownMenu3.classList.remove('active-dropdown-menu');
+}
+
+//Chamando a função
+
+menuItem1.addEventListener('click', ShowDropdown1);
+menuItem2.addEventListener('click', ShowDropdown2);
+menuItem3.addEventListener('click', ShowDropdown3);
+
+dropdownMenu1.addEventListener('mouseleave', RemoveDropdown);
+dropdownMenu2.addEventListener('mouseleave', RemoveDropdown);
+dropdownMenu3.addEventListener('mouseleave', RemoveDropdown);
+
+//Menu mobile
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', (e) => {
+            nav.classList.toggle('active-menu-mobile');
+            toggle.classList.toggle('active-bx');
+        })
+    }
+}
+
+showMenu('bx', 'menu-mobile');
